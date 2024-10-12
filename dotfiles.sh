ee39f0c74bd913278c07af35ad4309053b538a46
victor4pinheiro <victor.pinheiro.soft@proton.me>
2024-10-12 16:38:14 -0300

    Add script to update dotfiles

---
+++ dotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+## Sway
+cp -rf $HOME/.config/sway $HOME/dotfiles/
+
+## Waybar
+cp -rf $HOME/.config/waybar $HOME/dotfiles/
+
+## Footer
+cp -rf $HOME/.config/foot $HOME/dotfiles/
+
+## Fish
+cp -rf $HOME/.config/fish/config.fish $HOME/dotfiles/fish/
+
+## Environment
+
+cp -rf /etc/environment $HOME/dotfiles/
+
+## Makepkg
+cp -rf /etc/makepkg.conf $HOME/dotfiles/
+cp -rf /etc/makepkg.conf.d $HOME/dotfiles/
+
+## Pacman
+cp -rf /etc/pacman.conf $HOME/dotfiles/
