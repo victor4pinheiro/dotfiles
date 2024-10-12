bbde165cc9dc34e0c97b1bfb16349b113a808608
victor4pinheiro <victor.pinheiro.soft@proton.me>
2024-10-12 16:37:18 -0300

    Update Sway

--- fish/conf.d/sway.fish
+++
@@ -1,2 +0,0 @@
-set TTY1 (tty)
-[ "$TTY1" = "/dev/tty1" ] && exec sway
--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,20 @@
+set TTY1 (tty)
+
+if status --is-login
+    eval $(keychain --eval --quiet id_ed25519 ~/.ssh/id_ed25519)
+    if test (tty) = /dev/tty1
+        exec sway
+    end
+end
+
 starship init fish | source
+alias ls="eza"
+alias install="sudo pacman -Syu"
+alias aur_install="yay -Syu"
 set -gx GPG_TTY (tty)
 set -gx NPM_CONFIG_PREFIX ~/.npm-global
 set -U fish_user_paths ~/.local/bin/ $fish_user_paths
 set PATH $PATH /home/arch/.npm-global/bin/
 source /opt/asdf-vm/asdf.fish
 source ~/.asdf/plugins/java/set-java-home.fish
+uv generate-shell-completion fish | source
